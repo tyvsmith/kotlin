b1ea795e6fd85db08f3889250d73f7aec0127806
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-07-26 15:58:48 +0300

    SignatureDumpingBuilderFactory: cleanup code

--- compiler/backend/src/org/jetbrains/kotlin/codegen/state/SignatureDumpingBuilderFactory.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/state/SignatureDumpingBuilderFactory.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.codegen.state
@@ -21,6 +10,8 @@ import org.jetbrains.kotlin.codegen.ClassBuilder
 import org.jetbrains.kotlin.codegen.ClassBuilderFactory
 import org.jetbrains.kotlin.codegen.DelegatingClassBuilder
 import org.jetbrains.kotlin.codegen.DelegatingClassBuilderFactory
+import org.jetbrains.kotlin.codegen.coroutines.unwrapInitialDescriptorForSuspendFunction
+import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptorWithVisibility
 import org.jetbrains.kotlin.renderer.DescriptorRenderer
@@ -30,15 +21,13 @@ import org.jetbrains.kotlin.resolve.jvm.diagnostics.MemberKind
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.RawSignature
 import org.jetbrains.org.objectweb.asm.FieldVisitor
 import org.jetbrains.org.objectweb.asm.MethodVisitor
-import org.jetbrains.kotlin.codegen.coroutines.unwrapInitialDescriptorForSuspendFunction
-import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import java.io.BufferedWriter
 import java.io.File
 
 
 class SignatureDumpingBuilderFactory(
-        builderFactory: ClassBuilderFactory,
-        val destination: File
+    builderFactory: ClassBuilderFactory,
+    val destination: File
 ) : DelegatingClassBuilderFactory(builderFactory) {
 
     companion object {
@@ -71,26 +60,49 @@ class SignatureDumpingBuilderFactory(
     }
 
 
-    private inner class SignatureDumpingClassBuilder(val origin: JvmDeclarationOrigin, val _delegate: ClassBuilder) : DelegatingClassBuilder() {
+    private inner class SignatureDumpingClassBuilder(val origin: JvmDeclarationOrigin, val _delegate: ClassBuilder) :
+        DelegatingClassBuilder() {
         override fun getDelegate() = _delegate
 
         private val signatures = mutableListOf<Pair<RawSignature, DeclarationDescriptor?>>()
         private lateinit var javaClassName: String
 
-        override fun defineClass(origin: PsiElement?, version: Int, access: Int, name: String, signature: String?, superName: String, interfaces: Array<out String>) {
+        override fun defineClass(
+            origin: PsiElement?,
+            version: Int,
+            access: Int,
+            name: String,
+            signature: String?,
+            superName: String,
+            interfaces: Array<out String>
+        ) {
             javaClassName = name
 
             super.defineClass(origin, version, access, name, signature, superName, interfaces)
         }
 
-        override fun newMethod(origin: JvmDeclarationOrigin, access: Int, name: String, desc: String, signature: String?, exceptions: Array<out String>?): MethodVisitor {
+        override fun newMethod(
+            origin: JvmDeclarationOrigin,
+            access: Int,
+            name: String,
+            desc: String,
+            signature: String?,
+            exceptions: Array<out String>?
+        ): MethodVisitor {
             signatures += RawSignature(name, desc, MemberKind.METHOD) to origin.descriptor?.let {
                 if (it is CallableDescriptor) it.unwrapInitialDescriptorForSuspendFunction() else it
             }
             return super.newMethod(origin, access, name, desc, signature, exceptions)
         }
 
-        override fun newField(origin: JvmDeclarationOrigin, access: Int, name: String, desc: String, signature: String?, value: Any?): FieldVisitor {
+        override fun newField(
+            origin: JvmDeclarationOrigin,
+            access: Int,
+            name: String,
+            desc: String,
+            signature: String?,
+            value: Any?
+        ): FieldVisitor {
             signatures += RawSignature(name, desc, MemberKind.FIELD) to origin.descriptor
             return super.newField(origin, access, name, desc, signature, value)
         }
@@ -100,26 +112,28 @@ class SignatureDumpingBuilderFactory(
             outputStream.append("\t{\n")
             origin.descriptor?.let {
                 outputStream.append("\t\t").appendNameValue("declaration", TYPE_RENDERER.render(it)).append(",\n")
-                (it as? DeclarationDescriptorWithVisibility)?.visibility?.let {
-                    outputStream.append("\t\t").appendNameValue("visibility", it.displayName).append(",\n")
+                (it as? DeclarationDescriptorWithVisibility)?.visibility?.let { visibility ->
+                    outputStream.append("\t\t").appendNameValue("visibility", visibility.displayName).append(",\n")
                 }
             }
             outputStream.append("\t\t").appendNameValue("class", javaClassName).append(",\n")
 
             outputStream.append("\t\t").appendQuoted("members").append(": [\n")
-            signatures.joinTo(outputStream, ",\n") { buildString {
-                val (signature, descriptor) = it
-                append("\t\t\t{")
-                descriptor?.let {
-                    (it as? DeclarationDescriptorWithVisibility)?.visibility?.let {
-                        appendNameValue("visibility", it.displayName).append(",\t")
-                    }
-                    appendNameValue("declaration", MEMBER_RENDERER.render(it)).append(", ")
+            signatures.joinTo(outputStream, ",\n") {
+                buildString {
+                    val (signature, descriptor) = it
+                    append("\t\t\t{")
+                    descriptor?.let {
+                        (it as? DeclarationDescriptorWithVisibility)?.visibility?.let { visibility ->
+                            appendNameValue("visibility", visibility.displayName).append(",\t")
+                        }
+                        appendNameValue("declaration", MEMBER_RENDERER.render(it)).append(", ")
 
+                    }
+                    appendNameValue("name", signature.name).append(", ")
+                    appendNameValue("desc", signature.desc).append("}")
                 }
-                appendNameValue("name", signature.name).append(", ")
-                appendNameValue("desc", signature.desc).append("}")
-            }}
+            }
             outputStream.append("\n\t\t]\n\t}")
 
             super.done()
@@ -127,24 +141,24 @@ class SignatureDumpingBuilderFactory(
     }
 }
 
-private fun Appendable.appendQuoted(value: String?): Appendable = value?.let { append('"').append(jsonEscape(it)).append('"') } ?: append("null")
+private fun Appendable.appendQuoted(value: String?): Appendable =
+    value?.let { append('"').append(jsonEscape(it)).append('"') } ?: append("null")
+
 private fun Appendable.appendNameValue(name: String, value: String?): Appendable = appendQuoted(name).append(": ").appendQuoted(value)
 
 private fun jsonEscape(value: String): String = buildString {
-    for (index in 0..value.length - 1) {
-        val ch = value[index]
-        when (ch) {
+    for (element in value) {
+        when (element) {
             '\b' -> append("\\b")
             '\t' -> append("\\t")
             '\n' -> append("\\n")
             '\r' -> append("\\r")
             '\"' -> append("\\\"")
             '\\' -> append("\\\\")
-            else -> if (ch.toInt() < 32) {
-                append("\\u" + Integer.toHexString(ch.toInt()).padStart(4, '0'))
-            }
-            else {
-                append(ch)
+            else -> if (element.toInt() < 32) {
+                append("\\u" + Integer.toHexString(element.toInt()).padStart(4, '0'))
+            } else {
+                append(element)
             }
         }
     }
